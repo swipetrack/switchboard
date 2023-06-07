e3c3ad7ce40ee97116f77b6defa08e82e6b25b34
Moony <8032848+moonymachine@users.noreply.github.com>
2023-06-07 17:26:29 -0500

    Update IUnityTicker.cs
    Added MIT license to IUnityTicker interface change.

--- interface/ticker/IUnityTicker.cs
+++ interface/ticker/IUnityTicker.cs
@@ -1,5 +1,6 @@
 
 // Copyright © 2023 SwipeTrack Solutions
+// The contents of this file are licensed under the MIT license in the file named MIT.txt or LICENSE.txt located in the nearest parent directory.
 
 namespace Switchboard
 {
