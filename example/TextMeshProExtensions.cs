23623162b6ced9c846122a76e972dd4381285b90
Ronnie Moon <rmoon@swipetrack.com>
2023-10-12 11:53:49 -0500

    Uncommented TextMeshProExtensions so the code is readable on GitHub examples.

--- example/TextMeshProExtensions.cs
+++ example/TextMeshProExtensions.cs
@@ -1,38 +1,38 @@
-//using System;
-//using TMPro;
+using System;
+using TMPro;
 
-// Uncomment if using TextMeshPro in order to set text without allocating garbage or updating text geometry when it hasn't changed.
+// Adds methods to set text without allocating garbage or updating text geometry when it hasn't changed.
 namespace Switchboard
 {
 	public static class TextMeshProExtensions
 	{
-		//private static char[] StaticBuffer => _buffer ??= new char[StringMaker.MaxCapacity];
-		//[ThreadStatic] // <- If you think these methods could be accessed from multiple threads. (Unlikely, can remove.)
-		//private static char[] _buffer;
+		private static char[] StaticBuffer => _buffer ??= new char[StringMaker.MaxCapacity];
+		[ThreadStatic] // <- If you think these methods could be accessed from multiple threads. (Unlikely, can remove.)
+		private static char[] _buffer;
 
-		//public static void SetText(this TMP_Text textComponent, StringMaker stringMaker)
-		//{
-		//	SetText(textComponent, stringMaker, null);
-		//}
+		public static void SetText(this TMP_Text textComponent, StringMaker stringMaker)
+		{
+			SetText(textComponent, stringMaker, null);
+		}
 
-		//public static void SetText(this TMP_Text textComponent, StringMaker stringMaker, char[] buffer)
-		//{
-		//	if(textComponent == null)
-		//		throw new ArgumentNullException(nameof(textComponent));
+		public static void SetText(this TMP_Text textComponent, StringMaker stringMaker, char[] buffer)
+		{
+			if(textComponent == null)
+				throw new ArgumentNullException(nameof(textComponent));
 
-		//	if(stringMaker == null)
-		//		throw new ArgumentNullException(nameof(stringMaker));
+			if(stringMaker == null)
+				throw new ArgumentNullException(nameof(stringMaker));
 
-		//	buffer ??= StaticBuffer;
+			buffer ??= StaticBuffer;
 
-		//	if(buffer.Length < stringMaker.Length)
-		//		throw new ArgumentException($"The {nameof(buffer)} is not long enough.", nameof(buffer));
+			if(buffer.Length < stringMaker.Length)
+				throw new ArgumentException($"The {nameof(buffer)} is not long enough.", nameof(buffer));
 
-		//	if(!stringMaker.Equals(textComponent.text))
-		//	{
-		//		stringMaker.CopyTo(new Span<char>(buffer));
-		//		textComponent.SetCharArray(buffer, 0, stringMaker.Length);
-		//	}
-		//}
+			if(!stringMaker.Equals(textComponent.text))
+			{
+				stringMaker.CopyTo(new Span<char>(buffer));
+				textComponent.SetCharArray(buffer, 0, stringMaker.Length);
+			}
+		}
 	}
 }
