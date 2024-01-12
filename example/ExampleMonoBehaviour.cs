e798b88c80f2fe0a828cec3a7fa837cead9f0d67
Ronnie Moon <rmoon@swipetrack.com>
2024-01-12 11:31:03 -0600

    Removed example namespace from example code, and added the MIT license to the example folder, in addition to the interface folder. I want it to be obvious that the example code is free to use as well, but I don't want to add the license to the root of the repository because I don't want to give anyone the false impression that the Switchboard plugin on the Unity Asset Store is free.

--- example/ExampleMonoBehaviour.cs
+++ example/ExampleMonoBehaviour.cs
@@ -4,88 +4,85 @@ using UnityEngine;
 // UnityEngine.ILogger does not, and is almost never referenced by anyone using UnityEngine.
 using ILogger = Switchboard.ILogger;
 
-namespace SwitchboardExample
+[RequireComponent(typeof(MeshRenderer))]
+public class ExampleMonoBehaviour : MonoBehaviour
 {
-	[RequireComponent(typeof(MeshRenderer))]
-	public class ExampleMonoBehaviour : MonoBehaviour
+	[Min(0.0f)]
+	public float LogFrequency = 0.5f;
+	private float Timer;
+
+	private ILogger Logger;
+	private IModel Model;
+
+	private MeshRenderer Renderer;
+
+	private void Awake()
 	{
-		[Min(0.0f)]
-		public float LogFrequency = 0.5f;
-		private float Timer;
+		Renderer = GetComponent<MeshRenderer>();
+	}
 
-		private ILogger Logger;
-		private IModel Model;
+	private void OnEnable()
+	{
+		IInjector injector = null;
 
-		private MeshRenderer Renderer;
-
-		private void Awake()
+		// Get Logger
+		Logger ??= (injector ??= InjectorLocator.GetInjector())?.Get<ILogger>();
+		if(Logger == null)
 		{
-			Renderer = GetComponent<MeshRenderer>();
+			enabled = false;
+			Debug.LogError("No Logger!");
+			return;
 		}
 
-		private void OnEnable()
+		// Get Model
+		Model ??= (injector ??= InjectorLocator.GetInjector())?.Get<IModel>();
+		if(Model == null)
+			Logger.LogWarning("Please add a Model to the DependencyInjector.");
+	}
+
+	private void OnDisable()
+	{
+		// Release the reference, and get a new one if enabled.
+		Model = null;
+	}
+
+	private void Update()
+	{
+		UpdateRenderer();
+		UpdateLog();
+	}
+
+	// All of the changes that update the position and properties of the renderer components in the scene come from the model data, yet it is a loosely coupled dependency upon an interface.
+	// This is not intended to imply that model data at the composition root should be moving MonoBehaviours around.
+	// It is only meant to demonstrate how it is possible to keep models at the root of your application as pure C# objects, loosely coupled to scene data treated as the view layer.
+	private void UpdateRenderer()
+	{
+		if(Model != null)
 		{
-			IInjector injector = null;
-
-			// Get Logger
-			Logger ??= (injector ??= InjectorLocator.GetInjector())?.Get<ILogger>();
-			if(Logger == null)
-			{
-				enabled = false;
-				Debug.LogError("No Logger!");
-				return;
-			}
-
-			// Get Model
-			Model ??= (injector ??= InjectorLocator.GetInjector())?.Get<IModel>();
-			if(Model == null)
-				Logger.LogWarning("Please add a Model to the DependencyInjector.");
+			transform.position = Model.Position;
+			if(Renderer.material.color != Model.Color)
+				Renderer.material.color = Model.Color;
 		}
+	}
 
-		private void OnDisable()
+	// These logs are written to a persistent file on disk without allocating any new objects for garbage collection.
+	private void UpdateLog()
+	{
+		Timer += Time.unscaledDeltaTime;
+		if(LogFrequency > 0.0f && Timer >= LogFrequency)
 		{
-			// Release the reference, and get a new one if enabled.
-			Model = null;
-		}
+			Timer -= LogFrequency;
+			if(Timer >= LogFrequency)
+				Timer = 0.0f;
 
-		private void Update()
-		{
-			UpdateRenderer();
-			UpdateLog();
-		}
-
-		// All of the changes that update the position and properties of the renderer components in the scene come from the model data, yet it is a loosely coupled dependency upon an interface.
-		// This is not intended to imply that model data at the composition root should be moving MonoBehaviours around.
-		// It is only meant to demonstrate how it is possible to keep models at the root of your application as pure C# objects, loosely coupled to scene data treated as the view layer.
-		private void UpdateRenderer()
-		{
 			if(Model != null)
 			{
-				transform.position = Model.Position;
-				if(Renderer.material.color != Model.Color)
-					Renderer.material.color = Model.Color;
-			}
-		}
-
-		// These logs are written to a persistent file on disk without allocating any new objects for garbage collection.
-		private void UpdateLog()
-		{
-			Timer += Time.unscaledDeltaTime;
-			if(LogFrequency > 0.0f && Timer >= LogFrequency)
-			{
-				Timer -= LogFrequency;
-				if(Timer >= LogFrequency)
-					Timer = 0.0f;
-
-				if(Model != null)
-				{
-					StringMaker stringMaker = StringMaker.ThreadStaticInstance;
-					stringMaker.Length = 0;
-					stringMaker.Append("Position: ").Append(Model.Position)
-						.Append(" Color: ").Append(Model.Color);
-					Logger.LogInformation(stringMaker);
-					stringMaker.Clear();
-				}
+				StringMaker stringMaker = StringMaker.ThreadStaticInstance;
+				stringMaker.Length = 0;
+				stringMaker.Append("Position: ").Append(Model.Position)
+					.Append(" Color: ").Append(Model.Color);
+				Logger.LogInformation(stringMaker);
+				stringMaker.Clear();
 			}
 		}
 	}
--- example/FloatExample.cs
+++ example/FloatExample.cs
@@ -3,104 +3,101 @@ using Switchboard;
 using UnityEngine;
 using ILogger = Switchboard.ILogger;
 
-namespace SwitchboardExample
+public class FloatExample : MonoBehaviour
 {
-	public class FloatExample : MonoBehaviour
+	private ILogger Logger;
+
+	private void OnEnable()
 	{
-		private ILogger Logger;
-
-		private void OnEnable()
+		// Get Logger
+		IInjector injector = null;
+		Logger ??= (injector = InjectorLocator.GetInjector())?.Get<ILogger>();
+		if(Logger == null)
 		{
-			// Get Logger
-			IInjector injector = null;
-			Logger ??= (injector = InjectorLocator.GetInjector())?.Get<ILogger>();
-			if(Logger == null)
-			{
-				Debug.LogError("No Logger!");
-				enabled = false;
-				return;
-			}
+			Debug.LogError("No Logger!");
+			enabled = false;
+			return;
 		}
+	}
 
-		private void Start()
-		{
-			StringMaker stringMaker = StringMaker.ThreadStaticInstance;
-			float floatValue;
-			double doubleValue;
+	private void Start()
+	{
+		StringMaker stringMaker = StringMaker.ThreadStaticInstance;
+		float floatValue;
+		double doubleValue;
 
-			Logger.LogInformation("The float.ToString() method cannot render more than 7 significant digits, except when the \"G9\" format specifier is used. The double.ToString() method cannot render more than 15 significant digits, except when the \"G17\" format spefifier is used. There are many floating-point numbers that can never be accurately represented by using ToString(). Here are some examples of numbers that cannot be rendered with ToString(). However, the Switchboard ConvertToText() and StringMaker.Append() methods can render these values accurately in any format you choose.");
+		Logger.LogInformation("The float.ToString() method cannot render more than 7 significant digits, except when the \"G9\" format specifier is used. The double.ToString() method cannot render more than 15 significant digits, except when the \"G17\" format spefifier is used. There are many floating-point numbers that can never be accurately represented by using ToString(). Here are some examples of numbers that cannot be rendered with ToString(). However, the Switchboard ConvertToText() and StringMaker.Append() methods can render these values accurately in any format you choose.");
 
-			floatValue = 1.4f;
+		floatValue = 1.4f;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
-				.Append("\nfloat.ConvertToText(): ").Append(floatValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
+			.Append("\nfloat.ConvertToText(): ").Append(floatValue);
+		Logger.LogInformation(stringMaker);
 
-			floatValue = 48103632896.0f;
+		floatValue = 48103632896.0f;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
-				.Append("\nfloat.ConvertToText(): ").Append(floatValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
+			.Append("\nfloat.ConvertToText(): ").Append(floatValue);
+		Logger.LogInformation(stringMaker);
 
-			floatValue = 0.0001220703125f;
+		floatValue = 0.0001220703125f;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
-				.Append("\nfloat.ConvertToText(): ").Append(floatValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
+			.Append("\nfloat.ConvertToText(): ").Append(floatValue);
+		Logger.LogInformation(stringMaker);
 
-			floatValue = 0.000000000000000000000000006462348535570528709932880406796584793482907116413116455078125f;
+		floatValue = 0.000000000000000000000000006462348535570528709932880406796584793482907116413116455078125f;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
-				.Append("\nfloat.ConvertToText(): ").Append(floatValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("float.ToString(): ").Append(floatValue.ToString())
+			.Append("\nfloat.ConvertToText(): ").Append(floatValue);
+		Logger.LogInformation(stringMaker);
 
-			floatValue = 48103632896.0f;
+		floatValue = 48103632896.0f;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("float.ToString(\"############\"): ").Append(floatValue.ToString("############"))
-				.Append("\nfloat.ConvertToText(): ").Append(floatValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("float.ToString(\"############\"): ").Append(floatValue.ToString("############"))
+			.Append("\nfloat.ConvertToText(): ").Append(floatValue);
+		Logger.LogInformation(stringMaker);
 
-			floatValue = 0.0000019073486328125f;
+		floatValue = 0.0000019073486328125f;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("float.ToString(\"0.####################\"): ").Append(floatValue.ToString("0.####################"))
-				.Append("\nfloat.ConvertToText(): ").Append(floatValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("float.ToString(\"0.####################\"): ").Append(floatValue.ToString("0.####################"))
+			.Append("\nfloat.ConvertToText(): ").Append(floatValue);
+		Logger.LogInformation(stringMaker);
 
-			doubleValue = 5192296858534827628530496329220096.0;
+		doubleValue = 5192296858534827628530496329220096.0;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("double.ToString(): ").Append(doubleValue.ToString())
-				.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("double.ToString(): ").Append(doubleValue.ToString())
+			.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
+		Logger.LogInformation(stringMaker);
 
-			doubleValue = 0.000000007450580596923828125;
+		doubleValue = 0.000000007450580596923828125;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("double.ToString(): ").Append(doubleValue.ToString())
-				.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("double.ToString(): ").Append(doubleValue.ToString())
+			.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
+		Logger.LogInformation(stringMaker);
 
-			doubleValue = 6456360425798342656.0;
+		doubleValue = 6456360425798342656.0;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("double.ToString(\"####################\"): ").Append(doubleValue.ToString("####################"))
-				.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("double.ToString(\"####################\"): ").Append(doubleValue.ToString("####################"))
+			.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
+		Logger.LogInformation(stringMaker);
 
-			doubleValue = 0.000000000000000444089209850062616169452667236328125;
+		doubleValue = 0.000000000000000444089209850062616169452667236328125;
 
-			stringMaker.Length = 0;
-			stringMaker.Append("double.ToString(\"0.####################################################\"): ").Append(doubleValue.ToString("0.####################################################"))
-				.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
-			Logger.LogInformation(stringMaker);
+		stringMaker.Length = 0;
+		stringMaker.Append("double.ToString(\"0.####################################################\"): ").Append(doubleValue.ToString("0.####################################################"))
+			.Append("\ndouble.ConvertToText(): ").Append(doubleValue);
+		Logger.LogInformation(stringMaker);
 
-			stringMaker.Clear();
-		}
+		stringMaker.Clear();
 	}
 }
